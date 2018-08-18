13d904be3083d9c504d181901b10e9a9184c74e0
Michael Kohl <citizen428@gmail.com>
2018-08-18 23:57:12 +0700

    Use created lifecycle hook to focus input

--- src/components/todo-input.js
+++ src/components/todo-input.js
@@ -3,6 +3,7 @@ import { h } from "hyperapp"
 const TodoInput = ({ text }) => (_state, actions) => (
   <input
     type="text"
+    oncreate={e => e.focus()}
     onkeyup={e => {
       e.keyCode === 13 ? actions.add() : actions.updateText(e.target.value)
     }}
